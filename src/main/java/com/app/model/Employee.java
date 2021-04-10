4de38805ea49eac7cbb6b5a135ba2a7c44a42aa9
shivam6898 <shivampatil727@gmail.com>
2021-04-11 01:55:22 +0530

    added lombok

--- src/main/java/com/app/model/Employee.java
+++ src/main/java/com/app/model/Employee.java
@@ -6,9 +6,18 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @XmlRootElement
 @Table(name="employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 	
 	
@@ -17,40 +26,4 @@ public class Employee {
 	private Integer empId;
 	private String empName;
 	private Double empSal;
-	public Employee() {
-		super();
-	}
-	public Employee(String empName, Double empSal) {
-		super();
-		this.empName = empName;
-		this.empSal = empSal;
-	}
-	public Integer getEmpId() {
-		return empId;
-	}
-	public void setEmpId(Integer empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public Double getEmpSal() {
-		return empSal;
-	}
-	public void setEmpSal(Double empSal) {
-		this.empSal = empSal;
-	}
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
-	}
-	
-	
-	
-	
-	
-
 }
